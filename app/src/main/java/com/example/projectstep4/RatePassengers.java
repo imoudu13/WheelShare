fd270950f23e245a53a263ec3a09bc348a495eeb
nickhaydu <warnado70@gmail.com>
2023-12-05 19:41:30 -0800

    added the rate driver and CurrentlyRiding activities

--- app/src/main/java/com/example/projectstep4/RatePassengers.java
+++ app/src/main/java/com/example/projectstep4/RatePassengers.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RatePassengers extends AppCompatActivity implements ratingAdapter.OnItemClickListener{
+public class RatePassengers extends AppCompatActivity implements RatingAdapter.OnItemClickListener{
     TextView selectedNameTextView;
     RatingBar bar;
     List<String> names;
